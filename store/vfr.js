be9a2ec7d785a986723a69abea4cce6880f9455a
sosso21 <sofianetop21.st@gmail.com>
2024-02-18 22:41:00 +0100

    😑 fix : coiffeur

--- store/vfr.js
+++ store/vfr.js
@@ -1,6 +1,6 @@
 export const state = () => ({
   data: {
-    title: "Amar Koulougli Coifeur",
+    title: "Amar Koulougli coiffeur",
     navLink: [
       {
         name: "Emplacement",
@@ -13,7 +13,8 @@ export const state = () => ({
         svg: "bi bi-at",
       },
     ],
-    descriptionHome: "Salon de coiffure pour homme. Soins capillaire ; cheveux et barbe, Accueil chaleureux et travail professionnel.",
+    descriptionHome:
+      "Salon de coiffure pour homme. Soins capillaire ; cheveux et barbe, Accueil chaleureux et travail professionnel.",
     address: {
       title: "Emplacement",
       address: {
@@ -24,7 +25,8 @@ export const state = () => ({
         cp: 15154,
         country: "Algérie.(Dz)",
       },
-      iframe: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3201.1398628968554!2d3.983952115285579!3d36.64708577998041!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x128dcf02916dddff%3A0xdd29a3894da19508!2sAmar%20Coulougli%20Le%20Coiffeur!5e0!3m2!1sfr!2sdz!4v1617183222918!5m2!1sfr!2sdz",
+      iframe:
+        "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3201.1398628968554!2d3.983952115285579!3d36.64708577998041!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x128dcf02916dddff%3A0xdd29a3894da19508!2sAmar%20Coulougli%20Le%20Coiffeur!5e0!3m2!1sfr!2sdz!4v1617183222918!5m2!1sfr!2sdz",
     },
     social: {
       title: "Réseaux Sociaux",
